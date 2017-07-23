306313ba87014d41cf48069a6fc85041cdfdc0c4
Meng Sun <sunmeng90@126.com>
2017-07-23 13:59:41 +0800

    TDD-Green

--- src/main/java/org/meng/tdd/tictactoe/TicTacToe.java
+++ src/main/java/org/meng/tdd/tictactoe/TicTacToe.java
@@ -4,5 +4,7 @@ package org.meng.tdd.tictactoe;
  * Created by meng_ on 7/23/2017.
  */
 public class TicTacToe {
-
+    public void play(int i, int i1) {
+        throw new RuntimeException();
+    }
 }
