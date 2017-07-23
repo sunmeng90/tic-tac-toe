ba5766fe3b0cc3da9ef35c6b5b1eed9091f8dc0d
Meng Sun <sunmeng90@126.com>
2017-07-23 14:41:01 +0800

    boundary validation on x axis and y axis as well as occupied point on board

--- src/main/java/org/meng/tdd/tictactoe/TicTacToe.java
+++ src/main/java/org/meng/tdd/tictactoe/TicTacToe.java
@@ -4,7 +4,20 @@ package org.meng.tdd.tictactoe;
  * Created by meng_ on 7/23/2017.
  */
 public class TicTacToe {
+    private Character[][] board = {
+            {'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}
+    };
+
     public void play(int x, int y) {
-        throw new RuntimeException();
+        if (x < 1 || x > 3) {
+            throw new RuntimeException();
+        } else if (y < 1 || y > 3) {
+            throw new RuntimeException();
+        }
+        if (board[x - 1][y - 1] != '\0') {
+            throw new RuntimeException();
+        } else {
+            board[x - 1][y - 1] = 'X';
+        }
     }
 }
