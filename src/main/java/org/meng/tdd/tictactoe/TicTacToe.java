345a93fc7c8f34e338a79097dbcd20d9f6e3eaf2
Meng Sun <sunmeng90@126.com>
2017-07-23 13:58:10 +0800

    TDD-Red

--- src/main/java/org/meng/tdd/tictactoe/TicTacToe.java
+++ src/main/java/org/meng/tdd/tictactoe/TicTacToe.java
@@ -1,16 +1,8 @@
 package org.meng.tdd.tictactoe;
 
-import org.omg.SendingContext.RunTime;
-
 /**
  * Created by meng_ on 7/23/2017.
  */
 public class TicTacToe {
-    public void play(int x, int y) {
-        if (x < 1 || x > 3) {
-            throw new RuntimeException("runtime exception");
-        }else if(y < 1 || y >3){
-            throw new RuntimeException("runtime exception");
-        }
-    }
+
 }
